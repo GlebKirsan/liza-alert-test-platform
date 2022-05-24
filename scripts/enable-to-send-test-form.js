e9723ef9cdd68bd4d1746072309530c3fa61fe16
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2022-05-24 21:46:34 +0300

    fix bugs, add button redo

--- scripts/enable-to-send-test-form.js
+++ scripts/enable-to-send-test-form.js
@@ -2,39 +2,30 @@
 const inputs = document.querySelectorAll(".test__input-checkbox");
 const radios = document.querySelectorAll(".test__input-radio");
 const buttonSubmitTest = document.querySelector(".test__button-submit");
-let radioCheckedStatus = false;
-let checkboxCheckedStatus = false;
+let isRadioChecked = false;
+let isCheckboxChecked = false;
+
+function toggleSubmitButton() {
+  if (isRadioChecked && isCheckboxChecked) {
+    buttonSubmitTest.classList.add("test__button-submit_enabled");
+    buttonSubmitTest.removeAttribute("disabled");
+  } else {
+    buttonSubmitTest.classList.remove("test__button-submit_enabled");
+    buttonSubmitTest.setAttribute("disabled", "disabled");
+  }
+}
 
 inputs.forEach(inputItem => {
   inputItem.addEventListener("change", function () {
-    checkboxCheckedStatus = false;
-    // buttonSubmitTest.setAttribute("disabled", "disabled");
-    // buttonSubmitTest.classList.remove("test__button-submit_enable");
-    if (this.checked) {
-      checkboxCheckedStatus = true;
-    }
-    if (this.checked && radioCheckedStatus) {
-      buttonSubmitTest.classList.add("test__button-submit_enable");
-      buttonSubmitTest.removeAttribute("disabled");
-    } else if (!radioCheckedStatus) {
-      buttonSubmitTest.classList.remove("test__button-submit_enable");
-      buttonSubmitTest.setAttribute("disabled", "disabled");
-    }
+    isCheckboxChecked = this.checked;
+    toggleSubmitButton();
   });
 });
 
 radios.forEach(radioItem => {
   radioItem.addEventListener("change", function () {
-    radioCheckedStatus = false;
-    buttonSubmitTest.setAttribute("disabled", "disabled");
-    buttonSubmitTest.classList.remove("test__button-submit_enable");
-    if (this.checked) {
-      radioCheckedStatus = true;
-    }
-    if (this.checked && checkboxCheckedStatus) {
-      buttonSubmitTest.classList.add("test__button-submit_enable");
-      buttonSubmitTest.removeAttribute("disabled");
-    }
+    isRadioChecked = this.checked;
+    toggleSubmitButton();
   });
 });
 
